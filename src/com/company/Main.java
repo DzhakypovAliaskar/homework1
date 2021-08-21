54a2e1a2f097605dba8d2bad641ee6f39ce1970d
DzhakypovAliaskar <dzhakypov007@gmail.com>
2021-08-21 18:33:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+   public static int age = 25, temp = 15;
+
+    public static void main(String[] args) {
+
+
+        if (age < 45 && age > 10 && temp <30 && temp > -20)
+        {
+            System.out.println("можно идти гулять!");
+        }
+        else if (age > 10 && temp < 0) {
+
+        System.out.println("можно гулять но не долго!");
+        }
+
+        else if (65 > age && temp > -10)
+        { System.out.println("лучше остаться дома!");
+        }
+        else {
+            System.out.println("лучше остаться дома!");
+        }
+
+
+
+
+
+
+        }
+
+    }
+
